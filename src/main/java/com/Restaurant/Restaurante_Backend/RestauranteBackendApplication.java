11310f89e87f21eba2073773e32abadb453d32c7
jackutp <u21318614@utp.edu.pe>
2026-05-03 15:54:21 -0500

    first commit

---
+++ src/main/java/com/Restaurant/Restaurante_Backend/RestauranteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Restaurant.Restaurante_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteBackendApplication.class, args);
+	}
+
+}
