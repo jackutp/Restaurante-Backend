fb788c38cb31d57d21fddb5cf90a0198a7fe2627
jackutp <u21318614@utp.edu.pe>
2026-05-04 17:24:51 -0500

    Creado microservicio proveedor

---
+++ microservicio-proveedor/src/test/java/com/microservicio/proveedor/MicroservicioProveedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.proveedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioProveedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
