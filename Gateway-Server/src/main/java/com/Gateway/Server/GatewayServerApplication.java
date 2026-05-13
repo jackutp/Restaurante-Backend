6bea4896cb7e208c680017164b2d0eb305f583ee
italo <riositalo2@gmail.com>
2026-05-13 18:34:16 -0500

    small tweaking before the .env configurations

--- Gateway-Server/src/main/java/com/Gateway/Server/GatewayServerApplication.java
+++ Gateway-Server/src/main/java/com/Gateway/Server/GatewayServerApplication.java
@@ -1,4 +1,4 @@
-package com.Restaurant.gateway;
+package com.Gateway.Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayApplication {
+public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
